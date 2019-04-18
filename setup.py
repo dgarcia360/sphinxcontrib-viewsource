f13a17d0f3b0f9079be869714ce7bd224ed35a6f
dgarcia360 <dgarcia360@outlook.com>
2019-04-18 01:56:53 +0100

    Added 0.1.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinxcontrib-viewsource',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/dgarcia360/sphinxcontrib-viewsource',
     # download_url='http://pypi.python.org/pypi/sphinxcontrib-viewsource',
     license='MIT',
